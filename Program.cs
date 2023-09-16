7df877be41d31bc385a69c8a78f56d8fa0d33806
RusUsf <ruslan@usf.edu>
2023-09-16 12:09:38 -0400

    Practiced Quastions 1 to 5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// Q1: How to write a "Hello, World!" program?
+//Console.WriteLine("Hello, world!");
+//Console.WriteLine("I am capable of learning C# programming language. I am smart, intelligent, and hardworking. I have the ability to learn new things, including C#");
+
+//// Q2: How to declare and initialize an integer variable?
+//int myIntegerOne = 37;
+//Console.WriteLine($"The value of myIntegerOne: {myIntegerOne+1}");
+
+//// Q3: How to perform a basic conditional check using an if statement?
+
+//Console.WriteLine("Please, enter your age:");
+//string myIntegerTwo = Console.ReadLine();
+//int myIntegerTree = int.Parse(myIntegerTwo);
+//if (myIntegerTree == 37)
+//    Console.WriteLine($"The value of the myIntegerTwo: {myIntegerTree}");
+//else
+//    Console.WriteLine("The value of the myIntegerTwo is not 37!");
+
+//// Q4: What is the correct way to define a method that adds two numbers?
+//Console.WriteLine($"The sum of 3 and 5 is: {myAddTwoNumbers(3, 5)}");
+//static int myAddTwoNumbers(int numOne, int numTwo)
+//{
+//    return numOne + numTwo;
+//}
+
+//// Q5: How can you loop through an array of integers using a foreach loop?
+//int[] myArrayOne = new int[]{ 1, 2, 3, 4, 5, 6 };
+//string[] myArrayTwo = new string[] { "Curiosity", "Courage", "Creativity" };
+
+//Console.WriteLine("\n\nmyArrayOne:");
+//foreach (int i in myArrayOne)
+//    Console.Write(i+", ");
+//Console.WriteLine("\n\nmyArrayTwo:");
+//foreach (string j in myArrayTwo)
+//    Console.Write(j+", ");
+//Console.WriteLine("\n\nUsing string.Join() method");
+//Console.WriteLine(string.Join(", ", myArrayOne));
+//Console.WriteLine(string.Join(", ",myArrayTwo));
+    
+
+// Q6: How can you define a class with a property named Age?
+
+// Q7: How do you create an object of the class Person?
+// Q8: How do you handle exceptions using try-catch?
+// Q9: How do you read a line of input from the console?
+// Q10: How can you define an enumeration?
+// Q11: How can you concatenate strings?
+// Q12: What is a correct way to define a constant variable?
+// Q13: How do you write a switch statement?
+// Q14: How do you declare and use a delegate?
+// Q15: How do you define and call a lambda expression?
+// Q16: How can you instantiate a list of integers?
+// Q17: How do you convert a string to an integer?
+// Q18: How do you define a structure?
+// Q19: How can you define and start a new thread?
+// Q20: How to define an interface?
+// Q21: How to use LINQ to query a collection of integers?
+// Q22: How to asynchronously read a text file?
+// Q23: How to create a property with a private setter?
+// Q24: How to handle different exceptions using multiple catch blocks?
+// Q25: How to use string interpolation?
+// Q26: How to use the 'using' statement for automatic resource management?
+// Q27: How to use extension methods?
+// Q28: How to use indexers in a class?
+// Q29: How to use named and optional parameters in a method?
+// Q30: How to use attributes in C#?
+// Q31: How to declare and use a nullable value type?
+// Q32: How to implement the Singleton pattern in C#?
+// Q33: How to use the 'yield' keyword to create an iterator?
+// Q34: How to implement a generic method?
+// Q35: How to perform a deep copy of an object?
+// Q36: How to check if a string contains another string?
+// Q37: How to use the 'async' and 'await' keywords for asynchronous programming?
+// Q38: How to implement event handling in C#?
+// Q39: How to use the 'ref' keyword to pass a parameter by reference?
+// Q40: How to declare and use an anonymous type?
